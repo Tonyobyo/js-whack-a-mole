a2e2bf6602eb09a639ffb050c51676dd150235cf
Tonyobyo <michael.mastrucci@gmail.com>
2020-04-21 13:09:50 -0400

    Completed
    Cleaned up HTML UI and linked js correctly

--- js/app.js
+++ js/app.js
@@ -1,55 +1,48 @@
-//Selects all divs with square class, mole class, and time-left id, storing in const (immutable) variables
-const square = document.querySelectorAll('.square');
-const mole = document.querySelectorAll('.mole');
-const timeLeft = document.querySelector('#time-left');
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
 
-let score = document.querySelector('#score');
-let result = 0;
-let currentTime = timeLeft.textContent;
+let result = 0
+let currentTime = timeLeft.textContent
 
-//Randomly selects square on grid
 function randomSquare() {
-  //Arrow function removes any mole class from divs so there are no divs left with styling on the board
   square.forEach(className => {
-    className.classList.remove('mole');
+    className.classList.remove('mole')
   })
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole')
 
-  //Defines random position on grid to place mole - multiply by number of squares on grid
-  //Math.floor rounds down to nearest integer
-  let randomPosition = square[Math.floor(Math.random() * 9)];
-  //Once random position is selected, mole class is added
-  randomPosition.classList.add('mole');
-
-  //Assigns id of randomPosition to hitPosition
+  //assign the id of the randomPosition to hitPosition for us to use later
   hitPosition = randomPosition.id
 }
 
 square.forEach(id => {
-  //Refers to when we hit the mouse on an element (square on the grid)
   id.addEventListener('mouseup', () => {
-    if(id.id === hitPosition) {
-      result = result + 1;
-      score.textContent = result;
+    if(id.id === hitPosition){
+      result = result + 1
+      score.textContent = result
     }
   })
 })
 
+
 function moveMole() {
-  let timerId = null;
-  timerId = setInterval(randomSquare, 1000);
+  let timerId = null
+  timerId = setInterval(randomSquare, 500)
 }
 
-moveMole();
+moveMole()
 
-//Count down as game is played. Once it reaches 0, game over and score is displayed
+
 function countDown() {
-  currentTime--;
-  timeLeft.textContent = currentTime;
+  currentTime--
+  timeLeft.textContent = currentTime
 
-  if(currentTime === 0){
-    clearInterval(timerId);
-    alert('Game Over! Final score: ' + result);
+  if(currentTime === 0 ) {
+    clearInterval(timerId)
+    alert('GAME OVER! Your final score is' + result)
   }
 }
 
-let timerId = setInterval(countDown, 1000);
+let timerId = setInterval(countDown, 1000)
